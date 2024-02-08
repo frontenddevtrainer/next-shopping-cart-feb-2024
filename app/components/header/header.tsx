622731cb1408626816efbc773819970aa2b41583
Admin <admin@example.com>
2024-02-08 07:09:25 +0000

    Header menu popup

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,4 +1,10 @@
+"use client";
+import { useState } from "react";
+import HeaderPopup from "../header-popup/header-popup";
+
 const Header = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
     <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
       <h1 className="text-2xl">Music Store</h1>
@@ -8,41 +14,18 @@ const Header = () => {
         </a>
 
         <div className="relative inline-block text-left">
-          <button type="button" className="inline-flex items-center">
+          <button
+            onClick={() => {
+              setShowPopup(!showPopup);
+            }}
+            type="button"
+            className="inline-flex items-center"
+          >
             <span className="material-icons"> account_circle </span>
             <span className="ml-2">Profile</span>
             <span className="material-icons"> arrow_drop_down </span>
           </button>
-          <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-            <div
-              className="py-1"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="options-menu"
-            >
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-              >
-                Your Profile
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-              >
-                Settings
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-              >
-                Sign out
-              </a>
-            </div>
-          </div>
+          {showPopup && <HeaderPopup />}
         </div>
       </div>
     </header>
